3abc7f25ce05803280d501551a6d583a0f2ce7ed
rajeshkumar-coding <rajeshkumarpunniyakotti41@gmail.com>
2026-05-31 19:41:25 +0530

    CreateLead

---
+++ CreateLead.java
@@ -0,0 +1,57 @@
+package org.testleaf.SeleniumLearing_VSCode.Week2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateLead {
+public static void main(String[] args) {
+    ChromeOptions opt = new ChromeOptions();
+    opt.addArguments("--incognito");
+    ChromeDriver driver = new ChromeDriver(opt);
+    //opt.addArguments("guest");
+    //ChromeDriver driver=new ChromeDriver(opt);
+    driver.get("http://leaftaps.com/opentaps/control/main");
+    driver.manage().window().maximize();
+    WebElement username = driver.findElement(By.id("username"));
+    WebElement password = driver.findElement(By.id("password"));
+    WebElement login = driver.findElement(By.className("decorativeSubmit"));
+    username.sendKeys("DemoCSR"); 
+    password.sendKeys("crmsfa");
+    login.click();
+    WebElement crmsfa = driver.findElement(By.linkText("CRM/SFA"));
+    crmsfa.click();
+    WebElement leads = driver.findElement(By.linkText("Leads"));
+    leads.click();
+    WebElement createlead = driver.findElement(By.linkText("Create Lead"));
+    createlead.click();
+    WebElement company = driver.findElement(By.id("createLeadForm_companyName"));
+    company.sendKeys("FC Global Services India LLP");
+    WebElement Firstname = driver.findElement(By.id("createLeadForm_firstName"));
+    Firstname.sendKeys("Rajeshkumar");
+    WebElement lastname = driver.findElement(By.id("createLeadForm_lastName"));
+    lastname.sendKeys("Punniyakotti");
+    WebElement source = driver.findElement(By.id("createLeadForm_dataSourceId"));
+    Select sourceopt = new Select(source);
+    sourceopt.selectByIndex(5);
+    WebElement marketc = driver.findElement(By.id("createLeadForm_marketingCampaignId"));
+    Select marketcopt = new Select(marketc);
+    marketcopt.selectByVisibleText("Catalog Generating Marketing Campaigns");
+    WebElement impnote = driver.findElement(By.id("createLeadForm_importantNote"));
+    impnote.sendKeys("Automation Testing in Selenium using Java");
+    WebElement submitbtn = driver.findElement(By.name("submitButton"));
+    submitbtn.click();
+    String title = driver.getTitle();
+    System.out.println(title);
+    if (title.equals("View Lead | opentaps CRM"))
+    {
+        System.out.println("Test Passed");
+    }
+    else
+         System.out.println("Test Failed");
+         driver.close();
+  }
+}
+
+
